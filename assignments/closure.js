a0b5fab4526bf1dbb43f745d7b31ff3793872061
Crystal Martin <drcrystalm@gmail.com>
2019-04-15 12:55:57 -0500

    finished basic closure plus one stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,40 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function callName(firstName, lastName) {
+  const intro = "My name is ";
+
+  function makeFullName() {
+    return intro + firstName + " " + lastName;
+  }
+  return makeFullName();
+}
+
+console.log(callName("Crystal", "Martin"));
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+  let start = 0
+  const myFunction = () => {
+    start = start + 1
+    return start
+  }
+  return myFunction
+}
+
+const increment = counter();
+const c1 = increment(1);
+const c2 = increment(2);
+const c3 = increment(3);
+console.log(c1, c2, c3);
+
+
+// Return a function that when invoked increments and returns a counter variable.
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
